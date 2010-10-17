fff517f1777125b14fd82cdd1e2b342b7fc87e38
Vladimir Rutsky <altsysrq@gmail.com>
2010-10-17 18:41:13 +0400

    Add generated by NetBeans project

---
+++ Computational_geometry/Delaunay_triangulation/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: bob
+ *
+ * Created on October 17, 2010, 6:36 PM
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
