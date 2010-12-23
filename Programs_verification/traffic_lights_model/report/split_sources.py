17d2ea456f526b37bfbaa7353ccf1d4b0014b2b8
Vladimir Rutsky <altsysrq@gmail.com>
2010-12-23 09:02:51 +0300

    Splitting script

--- Programs_verification/traffic_lights_model/report/split_sources.py
+++ Programs_verification/traffic_lights_model/report/split_sources.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
-with open("data/mail.pml", 'r') as f:
+with open("data/main.pml", 'r') as f:
     contents = f.read()
 
-contents.split("\n/*** cut here ***/\n")
+parts = contents.split("/*** cut here ***/")
+assert len(parts) == 6
+
+locations = {
+    0: "data/begin.pml_",
+    1: "data/car_generators.pml_",
+    2: "data/intersections.pml_",
+    3: "data/traffic_lights.pml_",
+    4: "data/main.pml_",
+    5: "data/correctness.pml_",
+    }
+
+for idx, path in locations.iteritems():
+    with open(path, 'w') as f:
+        f.write(parts[idx].strip("\n"))
