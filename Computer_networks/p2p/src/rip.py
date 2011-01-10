f3ef377cab086fbb37c90ffa099462ee6ef72692
Vladimir Rutsky <altsysrq@gmail.com>
2011-01-10 03:55:04 +0300

    Add single router network tests. Fix some bugs

--- Computer_networks/p2p/src/rip.py
+++ Computer_networks/p2p/src/rip.py
@@ -59,7 +59,7 @@ class RIPService(object):
         self._link_manager = router_link_manager
         self._service_transmitter = service_transmitter
 
-        self._dest_to_next_router = {}
+        self._dest_to_next_router = {router_name: router_name}
         self._dest_to_next_router_lock = threading.Lock()
 
         # If working thread will be able to acquire the lock, then it should
@@ -84,9 +84,15 @@ class RIPService(object):
         def update_connected_routers_info():
             new_connected_routers = \
                 connected_routers - prev_connected_routers
-            still_connected_routers = connected_routers & prev_connected_routers
             new_disconnected_routers = \
                 prev_connected_routers - connected_routers
+
+            if new_connected_routers:
+                logger.debug("Connected to routers: {0}".format(
+                    new_connected_routers))
+            if new_disconnected_routers:
+                logger.debug("Disconnected from routers: {0}".format(
+                    new_disconnected_routers))
 
             # Remove disconnected routers from list.
             for router_name in new_disconnected_routers:
@@ -96,7 +102,7 @@ class RIPService(object):
             # Set distance to infinity for destination routers route to which
             # leaded through disconnected routers.
             for to_router, dest_router_info in dest_routers_info.iteritems():
-                if dest_router_info.next_router == to_router:
+                if dest_router_info.next_router in new_disconnected_routers:
                     dest_router_info.dist = self._inf_dist
 
             # Add connected hosts to according list.
@@ -188,7 +194,8 @@ class RIPService(object):
                 for dest, dest_rr_info in dest_routers_info.iteritems():
                     if dest_rr_info.dist < self._inf_dist:
                         assert dest not in self._dest_to_next_router
-                        assert dest_rr_info.next_router in connected_routers
+                        assert (dest_rr_info.next_router in connected_routers or
+                            dest_rr_info.next_router == self._router_name)
                         self._dest_to_next_router[dest] = \
                             dest_rr_info.next_router
 
@@ -225,7 +232,7 @@ class RIPService(object):
             prev_connected_routers = connected_routers
             connected_routers = \
                 frozenset(self._link_manager.connected_routers())
-
+                
             # Remove information about disconnected routers and add information
             # about newly connected routers.
             update_connected_routers_info()
@@ -285,12 +292,74 @@ def _test():
             def test_main(self):
                 rs1 = RIPService(1, self.lm1, self.rip_st1)
 
-                self.assertEqual(rs1.dynamic_routing_table().next_router(1),
+                self.assertEqual(rs1.dynamic_routing_table().next_router(1), 1)
+                self.assertEqual(rs1.dynamic_routing_table().next_router(2),
                     None)
 
                 rs1.terminate()
 
             def tearDown(self):
+                self.sm1.terminate()
+                self.dt1.terminate()
+
+        class TestRIPService1(unittest.TestCase):
+            def setUp(self):
+                self.lm1 = RouterLinkManager()
+
+                self.dt1 = DatagramRouter(
+                    router_name=1,
+                    link_manager=self.lm1)
+
+                self.sm1 = RouterServiceManager(self.dt1)
+                self.rip_st1 = self.sm1.register_service(520)
+
+                self.rs1 = RIPService(1, self.lm1, self.rip_st1,
+                    update_period=0.3, inf_timeout=0.6, remove_timeout=1)
+
+            def test_dynamic_routing(self):
+                self.dt1.set_routing_table(self.rs1.dynamic_routing_table())
+
+                time.sleep(5)
+
+            def test_dynamic_routing_transfer(self):
+                self.dt1.set_routing_table(self.rs1.dynamic_routing_table())
+                #time.sleep(0.5)
+
+                s10 = self.sm1.register_service(10)
+                s15 = self.sm1.register_service(15)
+
+                unreach_p10 = Packet(1, 2, "unreachable test (10)")
+                s10.send(unreach_p10)
+
+                p10_1 = Packet(1, 1, "test (10)")
+                s10.send(p10_1)
+
+                self.assertEqual(s10.receive(), p10_1)
+
+                self.assertEqual(s10.receive(block=False), None)
+
+                p15_1 = Packet(1, 1, "test (15)")
+                p10_2 = Packet(1, 1, "test 2 (10)")
+
+                s10.send(p10_1)
+                s15.send(p15_1)
+                s10.send(p10_2)
+
+                self.assertEqual(s15.receive(), p15_1)
+                self.assertEqual(s15.receive(block=False), None)
+                self.assertEqual(s10.receive(), p10_1)
+                self.assertEqual(s10.receive(), p10_2)
+                self.assertEqual(s10.receive(block=False), None)
+
+                text = "".join(map(chr, xrange(256)))
+                p15_2 = Packet(1, 1, text * 10)
+                s15.send(p15_2)
+                s10.send(p10_1)
+                self.assertEqual(s10.receive(), p10_1)
+                self.assertEqual(s15.receive(), p15_2)
+
+            def tearDown(self):
+                self.rs1.terminate()
                 self.sm1.terminate()
                 self.dt1.terminate()
 
