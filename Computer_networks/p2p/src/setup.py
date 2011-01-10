4c00d52a24091913bdf1405ff760bf2d9ce9c36f
Vladimir Rutsky <altsysrq@gmail.com>
2011-01-11 02:23:31 +0300

    Add distutils' setup.py

---
+++ Computer_networks/p2p/src/setup.py
@@ -0,0 +1,28 @@
+#  This file is part of network emulation test model.
+#
+#  Copyright (C) 2010, 2011  Vladimir Rutsky <altsysrq@gmail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+setup(
+    name='network_model',
+    version='0.1',
+    description="Network emulation test model",
+    author='Vladimir Rutsky',
+    author_email='altsysrq@gmail.com',
+    url='http://github.com/rutsky/semester09/',
+    py_modules = ['main'],
+    )
