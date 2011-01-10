91cee4c64430575742a7376ac25b573b99673176
Vladimir Rutsky <altsysrq@gmail.com>
2011-01-11 02:28:09 +0300

    Add note about not completeness of setup.py

--- Computer_networks/p2p/src/setup.py
+++ Computer_networks/p2p/src/setup.py
@@ -15,6 +15,9 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# TODO
+print "NOTE! setup.py is not complete and exists for testing purposes only!"
+
 from distutils.core import setup
 
 setup(
