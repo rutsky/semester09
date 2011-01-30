191d6410d419c84eabbbe4ea91d1a4f1958b8dc7
Vladimir Rutsky <altsysrq@gmail.com>
2011-01-31 02:08:16 +0300

    Add generator

---
+++ Computational_geometry/Delaunay_triangulation/tests/generator/generate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for n in `seq 1 99`; do
+  for i in `seq 1 3`; do
+    python uniform.py > ../uniform_`printf '%05d' $n`_$i.in
+  done
+done
+
+for n in `seq 100 100 5000`; do
+  for i in `seq 1 3`; do
+    python uniform.py > ../uniform_`printf '%05d' $n`_$i.in
+  done
+done
