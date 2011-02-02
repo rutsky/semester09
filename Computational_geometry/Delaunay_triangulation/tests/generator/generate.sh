33fb69bd54b18b6b862475b3dc6a9c085b328b8e
Vladimir Rutsky <altsysrq@gmail.com>
2011-02-02 16:03:42 +0300

    Enable all tests generation

--- Computational_geometry/Delaunay_triangulation/tests/generator/generate.sh
+++ Computational_geometry/Delaunay_triangulation/tests/generator/generate.sh
@@ -6,8 +6,8 @@ for n in `seq 1 99`; do
   done
 done
 
-#for n in `seq 100 100 5000`; do
-#  for i in `seq 1 3`; do
-#    python uniform.py $n > ../uniform_`printf '%05d' $n`_$i.in
-#  done
-#done
+for n in `seq 100 100 5000`; do
+  for i in `seq 1 5`; do
+    python uniform.py $n > ../uniform_`printf '%05d' $n`_$i.in
+  done
+done
