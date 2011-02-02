ee158a2d50b6f6e98479cdd025d1cedcf7ac5154
Vladimir Rutsky <altsysrq@gmail.com>
2011-02-02 04:16:25 +0300

    Add infinite testing routine

---
+++ Computational_geometry/Delaunay_triangulation/tests/generator/infinite_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#set -x
+
+APP=../../build/delaunay/delaunay
+VERIFICATOR=../../build/verificator/verificator
+N=10
+TEST_IN=test.in
+TEST_OUT=test.out
+
+i=1
+while true; do
+  echo $i
+  let i=$i+1
+  python uniform.py $N > $TEST_IN;
+  cat $TEST_IN | $APP > $TEST_OUT || exit 1
+
+  $VERIFICATOR $TEST_IN $TEST_OUT || exit 1
+done
