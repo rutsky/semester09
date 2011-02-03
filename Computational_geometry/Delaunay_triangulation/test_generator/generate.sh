8459780d98c03000feb087dc3db4a98e90ff640f
Vladimir Rutsky <altsysrq@gmail.com>
2011-02-03 15:18:11 +0300

    Add circle tests generation

--- Computational_geometry/Delaunay_triangulation/test_generator/generate.sh
+++ Computational_geometry/Delaunay_triangulation/test_generator/generate.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
-for n in `seq 1 99`; do
-  for i in `seq 1 3`; do
-    python uniform.py $n > ../tests/uniform_`printf '%05d' $n`_$i.in
+if false; then
+  # Small uniform tests.
+  for n in `seq 1 99`; do
+    for i in `seq 1 3`; do
+      python uniform.py $n > ../tests/uniform_`printf '%05d' $n`_$i.in
+    done
   done
-done
+fi
 
-for n in `seq 100 100 5000`; do
-  for i in `seq 1 5`; do
-    python uniform.py $n > ../tests/uniform_`printf '%05d' $n`_$i.in
+if false; then
+  # Big uniform tests.
+  for n in `seq 100 100 5000`; do
+    for i in `seq 1 5`; do
+      python uniform.py $n > ../tests/uniform_`printf '%05d' $n`_$i.in
+    done
   done
-done
+fi
+
+if true; then
+  # Circle tests.
+  for n in `seq 4 22`; do
+    python schinzel_circles.py $n > ../tests/circle_`printf '%03d' $n`.in
+  done
+fi
+
+# vim: set ts=2 sw=2 et:
