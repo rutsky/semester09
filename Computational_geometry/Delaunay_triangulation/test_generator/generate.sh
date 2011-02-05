358a0892a8783f4f55f068e908763782028d6bbd
Vladimir Rutsky <altsysrq@gmail.com>
2011-02-06 02:23:17 +0300

    Add points on circle tests generation with rbox

--- Computational_geometry/Delaunay_triangulation/test_generator/generate.sh
+++ Computational_geometry/Delaunay_triangulation/test_generator/generate.sh
@@ -27,18 +27,29 @@ if false; then
   done
 fi
 
-if true; then
+if false; then
   # Lattice points on small grid tests.
   for n in `seq 3 99`; do
     for i in `seq 1 5`; do
-      python lattice_uniform.py $n 0 7 0 7 > $TESTS_DIR/lattice_uniform_8x8_`printf '%03d' $n`_$i.in
+      python lattice_uniform.py $n 0 7 0 7 > \
+          $TESTS_DIR/lattice_uniform_8x8_`printf '%03d' $n`_$i.in
     done
   done
   for n in `seq 3 99`; do
     for i in `seq 1 5`; do
-      python lattice_uniform.py $n 0 19 0 2 > $TESTS_DIR/lattice_uniform_3x20_`printf '%03d' $n`_$i.in
+      python lattice_uniform.py $n 0 19 0 2 > \
+          $TESTS_DIR/lattice_uniform_3x20_`printf '%03d' $n`_$i.in
     done
   done
 fi
 
+if true; then
+  # Points on circle with rbox.
+  for n in `seq 5 5 100`; do
+    for i in `seq 1 2`; do
+      rbox $n s D2 | tail --lines=+3 - > \
+          $TESTS_DIR/circle_rbox_`printf '%03d' $n`_$i.in
+    done
+  done
+fi
 # vim: set ts=2 sw=2 et:
