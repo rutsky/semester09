c181043414f2edb61db12fcc22bec03496f231e2
Vladimir Rutsky <altsysrq@gmail.com>
2011-02-05 16:55:16 +0300

    Add lattice points tests generation

--- Computational_geometry/Delaunay_triangulation/test_generator/generate.sh
+++ Computational_geometry/Delaunay_triangulation/test_generator/generate.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+TESTS_DIR=../tests
+
 if false; then
   # Small uniform tests.
   for n in `seq 1 99`; do
     for i in `seq 1 3`; do
-      python uniform.py $n > ../tests/uniform_`printf '%05d' $n`_$i.in
+      python uniform.py $n > $TESTS_DIR/uniform_`printf '%05d' $n`_$i.in
     done
   done
 fi
@@ -13,15 +15,29 @@ if false; then
   # Big uniform tests.
   for n in `seq 100 100 5000`; do
     for i in `seq 1 5`; do
-      python uniform.py $n > ../tests/uniform_`printf '%05d' $n`_$i.in
+      python uniform.py $n > $TESTS_DIR/uniform_`printf '%05d' $n`_$i.in
     done
   done
 fi
 
-if true; then
+if false; then
   # Circle tests.
   for n in `seq 4 11` `seq 12 2 22`; do
-    python schinzel_circles.py $n > ../tests/circle_`printf '%03d' $n`.in
+    python schinzel_circles.py $n > $TESTS_DIR/circle_`printf '%03d' $n`.in
+  done
+fi
+
+if true; then
+  # Lattice points on small grid tests.
+  for n in `seq 3 99`; do
+    for i in `seq 1 5`; do
+      python lattice_uniform.py $n 0 7 0 7 > $TESTS_DIR/lattice_uniform_8x8_`printf '%03d' $n`_$i.in
+    done
+  done
+  for n in `seq 3 99`; do
+    for i in `seq 1 5`; do
+      python lattice_uniform.py $n 0 19 0 2 > $TESTS_DIR/lattice_uniform_3x20_`printf '%03d' $n`_$i.in
+    done
   done
 fi
 
