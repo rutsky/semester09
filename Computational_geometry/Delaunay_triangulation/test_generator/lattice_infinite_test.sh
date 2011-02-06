faaf28328f980e33f60a0457b538509f52d0b3f7
Vladimir Rutsky <altsysrq@gmail.com>
2011-02-06 06:05:40 +0300

    Update infinite test. Add infinite test for lattice points

--- Computational_geometry/Delaunay_triangulation/test_generator/lattice_infinite_test.sh
+++ Computational_geometry/Delaunay_triangulation/test_generator/lattice_infinite_test.sh
@@ -5,14 +5,14 @@
 APP=../build/delaunay/release/delaunay
 VERIFICATOR=../build/verificator/release/verificator
 N=100
-TEST_IN=test.in
-TEST_OUT=test.out
+TEST_IN=lattice_test.in
+TEST_OUT=lattice_test.out
 
 i=1
 while true; do
   echo $i
   let i=$i+1
-  python uniform.py $N > $TEST_IN;
+  python lattice_uniform.py $N 0 30 0 5 > $TEST_IN;
   cat $TEST_IN | $APP > $TEST_OUT || exit 1
 
   $VERIFICATOR $TEST_IN $TEST_OUT || exit 1
