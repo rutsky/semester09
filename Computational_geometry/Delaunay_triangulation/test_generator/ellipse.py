982d97c42466f5e9da7b16c1dd050b3fc6d347bf
Vladimir Rutsky <altsysrq@gmail.com>
2011-02-07 04:49:16 +0300

    Add points on ellipse generator

---
+++ Computational_geometry/Delaunay_triangulation/test_generator/ellipse.py
@@ -0,0 +1,19 @@
+import sys
+import random
+import math
+
+if len(sys.argv) == 2:
+    n = int(sys.argv[1])
+else:
+    print "Usage:\n  {0} <Number of points>".format(sys.argv[0])
+    sys.exit(0)
+
+a = 500
+b = 200
+for i in xrange(n):
+    t = random.uniform(0, 2 * math.pi)
+    x = a * math.cos(t)
+    y = b * math.sin(t)
+    print x, y
+
+# vim: set ts=4 sw=4 et: 
