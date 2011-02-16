4efdf0bab21f967288956e7c1d17cb9ebfdf95f2
Vladimir Rutsky <altsysrq@gmail.com>
2011-02-16 16:05:22 +0300

    Add preview into EPS, otherwise OpenOffice to slow on them
    See bug report: <http://qa.openoffice.org/issues/show_bug.cgi?id=77068>

--- Internet_technologies/unknown_words/specification/export_from_dia.sh
+++ Internet_technologies/unknown_words/specification/export_from_dia.sh
@@ -2,7 +2,10 @@
 
 for f in *.dia; do
   diagram=`echo $f | sed 's/\.dia$//'`
-  dia -e $diagram.eps $f
+  TEMP=`tempfile -s .eps`
+  dia -e $TEMP $f
+  epstool -i $TEMP $diagram.eps
+  rm $TEMP
 done
 
 # vim: set ts=2 sw=2 et:
