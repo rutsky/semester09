a07a126f5ead6a91903aca86aa6c4cf816eed945
Vladimir Rutsky <altsysrq@gmail.com>
2011-05-23 11:18:40 +0400

    Add first translation source

---
+++ Computer_networks/p2p/src/i18n/ru_RU.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="forms/main_window.ui" line="14"/>
+        <source>Network Model by Vladimir Rutsky, 5057/2, 2010-2011</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="30"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="39"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="42"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
