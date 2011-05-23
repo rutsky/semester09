357701c9dc7301d0dfcc7205459f3775706957dc
Vladimir Rutsky <altsysrq@gmail.com>
2011-05-23 11:20:32 +0400

    First translation

--- Computer_networks/p2p/src/i18n/ru_RU.ts
+++ Computer_networks/p2p/src/i18n/ru_RU.ts
@@ -1,26 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="forms/main_window.ui" line="14"/>
         <source>Network Model by Vladimir Rutsky, 5057/2, 2010-2011</source>
-        <translation type="unfinished"></translation>
+        <translation>Модель сети. Владимир Руцкий, 5057/2, 2010-2011</translation>
     </message>
     <message>
         <location filename="forms/main_window.ui" line="30"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Файл</translation>
     </message>
     <message>
         <location filename="forms/main_window.ui" line="39"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>В&amp;ыход</translation>
     </message>
     <message>
         <location filename="forms/main_window.ui" line="42"/>
         <source>Ctrl+Q</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Q</translation>
     </message>
 </context>
 </TS>
