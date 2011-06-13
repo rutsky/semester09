2faccb0dcb70f33d59be4486046a070022b79ef3
Vladimir Rutsky <altsysrq@gmail.com>
2011-06-14 01:13:25 +0400

    Update russian translation

--- Computer_networks/p2p/src/i18n/ru_RU.ts
+++ Computer_networks/p2p/src/i18n/ru_RU.ts
@@ -2,11 +2,30 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
+    <name>DockWidget</name>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="17"/>
+        <source>Control</source>
+        <translation>Управление</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="26"/>
+        <source>Number of routers:</source>
+        <translation>Количество роутеров:</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="33"/>
+        <source>0</source>
+        <translatorcomment>Количество роутеров по умолчанию</translatorcomment>
+        <translation>0</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="forms/main_window.ui" line="14"/>
         <source>Network Model by Vladimir Rutsky, 5057/2, 2010-2011</source>
-        <translation>Модель сети. Владимир Руцкий, 5057/2, 2010-2011</translation>
+        <translation type="obsolete">Модель сети. Владимир Руцкий, 5057/2, 2010-2011</translation>
     </message>
     <message>
         <location filename="forms/main_window.ui" line="30"/>
@@ -22,6 +41,11 @@
         <location filename="forms/main_window.ui" line="42"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="forms/main_window.ui" line="14"/>
+        <source>Network Model by Vladimir Rutsky, 5057/12, 2010-2011</source>
+        <translation>Модель сети. Владимир Руцкий, 5057/12, 2010-2011</translation>
     </message>
 </context>
 </TS>
