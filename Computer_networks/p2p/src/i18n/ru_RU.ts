834ca170aa8b1182a23f6b95610ea780bd01e0e0
Vladimir Rutsky <altsysrq@gmail.com>
2011-06-16 08:04:15 +0400

    Update translation

--- Computer_networks/p2p/src/i18n/ru_RU.ts
+++ Computer_networks/p2p/src/i18n/ru_RU.ts
@@ -16,8 +16,42 @@
     <message>
         <location filename="forms/main_dockable_panel.ui" line="33"/>
         <source>0</source>
-        <translatorcomment>Количество роутеров по умолчанию</translatorcomment>
         <translation>0</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="66"/>
+        <source>Stop Routers Motion</source>
+        <translation>Остановить роутеры</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="86"/>
+        <source>Shake Routers</source>
+        <translation>Толкнуть роутеры</translation>
+    </message>
+    <message>
+        <location filename="forms/transmission_widget.ui" line="17"/>
+        <source>Transmission</source>
+        <translation>Передача</translation>
+    </message>
+    <message>
+        <location filename="forms/transmission_widget.ui" line="26"/>
+        <source>Open Image</source>
+        <translation>Загрузить изображение</translation>
+    </message>
+    <message>
+        <location filename="forms/transmission_widget.ui" line="33"/>
+        <source>Initiate Transmission</source>
+        <translation>Начать передачу</translation>
+    </message>
+    <message>
+        <location filename="forms/transmission_widget.ui" line="46"/>
+        <source>Source image</source>
+        <translation>Исходное изображение</translation>
+    </message>
+    <message>
+        <location filename="forms/transmission_widget.ui" line="56"/>
+        <source>Transmitted image</source>
+        <translation>Переданное изображение</translation>
     </message>
 </context>
 <context>
@@ -46,6 +80,19 @@
         <location filename="forms/main_window.ui" line="14"/>
         <source>Network Model by Vladimir Rutsky, 5057/12, 2010-2011</source>
         <translation>Модель сети. Владимир Руцкий, 5057/12, 2010-2011</translation>
+    </message>
+</context>
+<context>
+    <name>TransmissionWidget</name>
+    <message>
+        <location filename="transmission_widget.py" line="121"/>
+        <source>Open Image</source>
+        <translation>Загрузить изображение</translation>
+    </message>
+    <message>
+        <location filename="transmission_widget.py" line="121"/>
+        <source>Image Files (*.png *.jpg *.jpeg *.bmp)</source>
+        <translation>Файлы изображений (*.png *.jpg *.jpeg *.bmp)</translation>
     </message>
 </context>
 </TS>
