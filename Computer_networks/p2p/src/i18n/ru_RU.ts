d8baccf1574135e1e99ab2518db0fc2a9cd63822
Vladimir Rutsky <altsysrq@gmail.com>
2011-06-17 17:29:55 +0400

    Update translation

--- Computer_networks/p2p/src/i18n/ru_RU.ts
+++ Computer_networks/p2p/src/i18n/ru_RU.ts
@@ -4,29 +4,29 @@
 <context>
     <name>DockWidget</name>
     <message>
-        <location filename="forms/main_dockable_panel.ui" line="17"/>
+        <location filename="forms/main_dockable_panel.ui" line="23"/>
         <source>Control</source>
         <translation>Управление</translation>
     </message>
     <message>
         <location filename="forms/main_dockable_panel.ui" line="26"/>
         <source>Number of routers:</source>
-        <translation>Количество роутеров:</translation>
+        <translation type="obsolete">Количество роутеров:</translation>
     </message>
     <message>
-        <location filename="forms/main_dockable_panel.ui" line="33"/>
+        <location filename="forms/main_dockable_panel.ui" line="169"/>
         <source>0</source>
         <translation>0</translation>
     </message>
     <message>
         <location filename="forms/main_dockable_panel.ui" line="66"/>
         <source>Stop Routers Motion</source>
-        <translation>Остановить роутеры</translation>
+        <translation type="obsolete">Остановить роутеры</translation>
     </message>
     <message>
         <location filename="forms/main_dockable_panel.ui" line="86"/>
         <source>Shake Routers</source>
-        <translation>Толкнуть роутеры</translation>
+        <translation type="obsolete">Толкнуть роутеры</translation>
     </message>
     <message>
         <location filename="forms/transmission_widget.ui" line="17"/>
@@ -52,6 +52,112 @@
         <location filename="forms/transmission_widget.ui" line="56"/>
         <source>Transmitted image</source>
         <translation>Переданное изображение</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="35"/>
+        <source>Connection distance&lt;sup&gt;1&lt;/sup&gt;:</source>
+        <translation>Расстояние соединения&lt;sup&gt;1&lt;/sup&gt;:</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="80"/>
+        <source>Average time for node to traverse whole scene (in&amp;nbsp;seconds):</source>
+        <translation>Среднее время за которое узел пролетает всю сцену (в&amp;nbsp;секундах):</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="122"/>
+        <source>Number of nodes:</source>
+        <translation>Число узлов:</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="158"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Disconnection distance&lt;span style=&quot; vertical-align:super;&quot;&gt;1&lt;/span&gt;:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Расстояние разъединения&lt;span style=&quot; vertical-align:super;&quot;&gt;1&lt;/span&gt;:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="200"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; vertical-align:super;&quot;&gt;1&lt;/span&gt; Ratio of scene half size.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; vertical-align:super;&quot;&gt;1&lt;/span&gt; Доля половины размера сцены.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="214"/>
+        <source>Render connection and disconnection ranges</source>
+        <translation>Рисовать зоны соединения и разъединения</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="236"/>
+        <source>Stop Nodes Motion</source>
+        <translation>Остановить движение узлов</translation>
+    </message>
+    <message>
+        <location filename="forms/main_dockable_panel.ui" line="256"/>
+        <source>Shake Nodes</source>
+        <translation>Запустить движение узлов</translation>
+    </message>
+    <message>
+        <location filename="forms/transmission_widget.ui" line="71"/>
+        <source>Success ratio:</source>
+        <translation>Доля успеха:</translation>
+    </message>
+    <message>
+        <location filename="forms/transmission_widget.ui" line="78"/>
+        <source>0.0 %</source>
+        <translation>0.0 %</translation>
+    </message>
+    <message>
+        <location filename="forms/statistics_widget.ui" line="23"/>
+        <source>Statistics</source>
+        <translation>Статистика</translation>
+    </message>
+    <message>
+        <location filename="forms/statistics_widget.ui" line="93"/>
+        <source>0.00 %</source>
+        <translation>0.00 %</translation>
+    </message>
+    <message>
+        <location filename="forms/statistics_widget.ui" line="42"/>
+        <source>Ratio of not optimal routes in routing tables:</source>
+        <translation>Доля неоптимальных путей в таблицах маршрутизации:</translation>
+    </message>
+    <message>
+        <location filename="forms/statistics_widget.ui" line="52"/>
+        <source>Ratio of incorrect routes in routing tables:</source>
+        <translation>Доля неправильных путей в таблицах маршрутизации:</translation>
+    </message>
+    <message>
+        <location filename="forms/statistics_widget.ui" line="69"/>
+        <source>Average topology changes frequency:</source>
+        <translation>Средняя интенсивность изменения топологии:</translation>
+    </message>
+    <message>
+        <location filename="forms/statistics_widget.ui" line="79"/>
+        <source>0 s</source>
+        <translation>0 с</translation>
+    </message>
+    <message>
+        <location filename="forms/statistics_widget.ui" line="107"/>
+        <source>(av.</source>
+        <translation>(ср.</translation>
+    </message>
+    <message>
+        <location filename="forms/statistics_widget.ui" line="121"/>
+        <source>)</source>
+        <translation>)</translation>
     </message>
 </context>
 <context>
@@ -81,6 +187,16 @@
         <source>Network Model by Vladimir Rutsky, 5057/12, 2010-2011</source>
         <translation>Модель сети. Владимир Руцкий, 5057/12, 2010-2011</translation>
     </message>
+    <message>
+        <location filename="main_window.py" line="260"/>
+        <source>{0:.2f} s</source>
+        <translation>{0:.2f} с</translation>
+    </message>
+    <message>
+        <location filename="main_window.py" line="381"/>
+        <source>{0:.2f} %</source>
+        <translation>{0:.2f} %</translation>
+    </message>
 </context>
 <context>
     <name>TransmissionWidget</name>
@@ -92,7 +208,12 @@
     <message>
         <location filename="transmission_widget.py" line="121"/>
         <source>Image Files (*.png *.jpg *.jpeg *.bmp)</source>
-        <translation>Файлы изображений (*.png *.jpg *.jpeg *.bmp)</translation>
+        <translation type="obsolete">Файлы изображений (*.png *.jpg *.jpeg *.bmp)</translation>
+    </message>
+    <message>
+        <location filename="transmission_widget.py" line="121"/>
+        <source>Image Files (*.png *.jpg *.jpeg *.bmp *.gif *.ico *.svg *.tiff)</source>
+        <translation>Image Files (*.png *.jpg *.jpeg *.bmp *.gif *.ico *.svg *.tiff)</translation>
     </message>
 </context>
 </TS>
