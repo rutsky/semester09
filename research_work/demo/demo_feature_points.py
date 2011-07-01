5e0263cf87262644014bce149bbb7294673f0c2a
Vladimir Rutsky <altsysrq@gmail.com>
2011-07-01 09:19:22 +0400

    Find and draw kaypoints

--- research_work/demo/demo_feature_points.py
+++ research_work/demo/demo_feature_points.py
@@ -7,12 +7,21 @@ from numpy import array
 
 def main():
     src_cv_img = cv.LoadImage("data/gorskaya/images/1/g126.jpg")
+    src_cv_img_gray = cv.LoadImage("data/gorskaya/images/1/g126.jpg", 
+        cv.CV_LOAD_IMAGE_GRAYSCALE)
+
+    (keypoints, descriptors) = \
+        cv.ExtractSURF(src_cv_img_gray, None, cv.CreateMemStorage(), 
+            (0, 30000, 3, 1))
+
+    print("Found {0} keypoints".format(len(keypoints)))
 
     src_arr = array(src_cv_img[:, :])[:, :, ::-1]
 
     pylab.rc('image', interpolation='nearest')
     pylab.subplot(111)
     pylab.imshow(src_arr)
+    pylab.plot(*[k[0] for k in keypoints], marker='o', color='r', ls='')
 
     pylab.show()
 
